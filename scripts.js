0976229e5cc430e61f664d6f04a1a83e7e357c1f
AbdallahAwaysheh <awaysheh6930@gmail.com>
2024-06-08 21:02:07 +0300

    Done

---
+++ scripts.js
@@ -0,0 +1,124 @@
+// Task 1
+// function task1 (){
+//     for (let i =1; i<=10; i++){
+//         console.log(i);
+//     }
+// }
+// task1();
+// ----------------------------------------
+// Task 2
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// ------------------------------
+// Task 4
+// for(let i = 0;i<=10; i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+// ---------------------------
+// Task 5
+// let sum =0;
+// for (let i =1; i<=10; i++){
+//     sum+=i;
+// }
+// console.log (sum);
+// ---------------------------------
+// Task 6
+// for(let i = 0;i<=10; i++){
+//     if (i%2!=0){
+//         console.log(i)
+//     }
+// }
+// -----------------------------
+// Task 7
+// let age =(prompt("How Old Are You?"));
+// if (age>16){
+//     alert("You are an adult");
+// }else{
+//     alert("You are Not adult");
+// }
+// ----------------------------
+// Task 8
+// let score =(prompt("What is Your Score?"));
+// if (score<=50){
+//     alert("You failed the test");
+// }else if(score<=100){
+//     alert("You Won!!");
+// }else{
+//     alert("Insert valid Number Please!!");
+// }
+// -----------------------------
+// Task 9
+// let userName = (prompt("What is your name ??"));
+// if (userName==="John"){
+//     alert("Hello, "+userName);
+// }else{
+//     alert("You Are not John!!");
+// }
+// -----------------------------------------
+// Task 10
+// let day = (prompt("What is your name ??"));
+// if (day != "Saturday"&&"Friday"){
+//     alert(day+" is  a weekday");
+// }else{
+//     alert("It's a weekEnd");
+// }
+// -----------------------------------------
+// Task 11
+// let num =prompt("What is the number");
+// if (Number(num)%2==0){
+//     alert("This Number is Even");
+// }else{
+//     alert("This Number is Odd");
+// }
+// -----------------------------------------
+// Task 12 & 13
+// let x =prompt("What is the Number");
+// if (x>0){
+//     alert(x+" is a positive number")
+// }else if (x==0){
+//     alert("hahahahaha Thats Funny")
+
+// }else{
+//     alert(x+ " is a negative Number")
+// }
+// -------------------------------------------
+// Task 14
+// let gpa = prompt("What is the Number");
+// if (gpa >= 3.5){
+//     alert("Congratulations, you have a good GPA!");
+// }
+// -------------------------------------------
+// Task 15
+// let isPrime = true;
+// let number = prompt("What is the number?");
+// if (number < 1) {
+//     console.log(`${number} is not prime`);
+//     isPrime = false;
+// }
+// else if (number == 1) {
+//     console.log(`1 is a prime number`);
+//     isPrime = true;
+// } else if (number > 1) {
+//     for (let i = 2; i < number; i++) {
+//         if (number% i == 0) {
+//             console.log(`${number} is not prime`);
+            
+//         }
+//     }
+// }else if (isPrime){
+//     console.log(`${number} is prime Number`);
+// }
+
+
+
+
+
+
+
+
+
